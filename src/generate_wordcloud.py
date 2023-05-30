e89a482c22e0a1296381a5fd4b2ecab2d0b000cb
叶璨铭 <12011404@mail.sustech.edu.cn>
2023-05-31 02:51:25 +0800

    feat: 停用词

--- src/generate_wordcloud.py
+++ src/generate_wordcloud.py
@@ -6,6 +6,10 @@ project_directory = this_directory.parent
 input_directory = project_directory/"texts"
 output_directory = project_directory/"wordclouds"
 #%%
+with open(this_directory/"stop_words.txt") as f:
+    stopwords = [i.strip() for i in f.readlines()]
+stopwords[-5:]
+#%%
 # https://zhuanlan.zhihu.com/p/353795160
 import tqdm, time
 import jieba, wordcloud
@@ -18,7 +22,7 @@ for txt_file in bar:
     path = txt_file.parent
     relative = path.relative_to(input_directory)
     new_path = output_directory/relative
-    new_path.mkdir(exist_ok=True)
+    new_path.mkdir(exist_ok=True, parents=True)
     new_file = new_path/f"{txt_file.stem}.png"
     if new_file.exists() and skip_exists:
         continue
@@ -29,12 +33,17 @@ for txt_file in bar:
     ls = jieba.lcut(s) # 生成分词列表
     text = ' '.join(ls) # 连接成字符串
     # 词云
-    stopwords = ["的","是","了"]
+    stopwords_new = ["的","是","了", 
+                 "就是", "这个", "然后",
+                 "说", "想", "做",
+                 "希望","是因为", "东西", 
+                 "采访者1", "采访者2", "受访者", "采访者"]+stopwords
+    stopwords_new = list(set(stopwords_new))
     wc = wordcloud.WordCloud(font_path="msyh.ttc",
                          width = 1000,
                          height = 700,
                          background_color='white',
-                         max_words=100,stopwords=s)
+                         max_words=100,stopwords=stopwords_new)
     # msyh.ttc电脑本地字体，写可以写成绝对路径
     wc.generate(text) # 加载词云文本
     wc.to_file(new_file) # 保存词云文件
